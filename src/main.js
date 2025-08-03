1ca1bd84260c3516bc7053cd75ff63938dbc3e29
Alex Shatokhin <shatokhin.04@inbox.ru>
2025-08-03 11:27:08 +0300

    fox bug with 100vh safari

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import { initAccordion } from "./js/components/accordion.js";
 import {toggleBurgerMenu} from "./js/components/burger.js";
 
 document.addEventListener('DOMContentLoaded', () => {
+    const appHeight = () => {
+        const doc = document.documentElement
+        doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+    }
+    window.addEventListener('resize', appHeight)
+    appHeight()
+    
     initAccordion();
     toggleBurgerMenu();
 });
